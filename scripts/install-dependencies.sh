f3164da22666f0a69f41faf831a0f88008b57923
tony-dinh <tonyd@sfu.ca>
2017-01-23 16:23:40 -0800

    ✨ delete & ignore vendor directory

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+VENDOR_DIR="${ROOT_DIR}/src/app/js/vendor/"
 
-echo 'Copying dependencies to ${ROOT_DIR}/src/js/vendor'
-cp $ROOT_DIR/node_modules/jquery/dist/jquery.min.js $ROOT_DIR/src/app/js/vendor/
-
-echo '🍻 Dependencies successfully installed'
+# Make vendor directory if it doesn't exist
+echo $VENDOR_DIR
+[ -d $VENDOR_DIR ] || mkdir $VENDOR_DIR
+#
+echo "[ 💿  ] Copying dependencies to $VENDOR_DIR"
+echo "[ .. ] Working..."
+cp $ROOT_DIR/node_modules/jquery/dist/jquery.min.js $VENDOR_DIR
+echo "[ OK ] Dependencies successfully installed 🍻!"
