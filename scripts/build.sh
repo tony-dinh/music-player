8628862fcc01564aa19abd1282b64cbd86ca86af
tony-dinh <tonyd@sfu.ca>
2017-01-23 17:04:03 -0800

    ✨ include build script

---
+++ scripts/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+
+pushd $ROOT_DIR
+webpack --config webpack.config.js
+sass $ROOT_DIR/src/app/styles/styles.scss:$ROOT_DIR/src/client/styles.css
+popd
