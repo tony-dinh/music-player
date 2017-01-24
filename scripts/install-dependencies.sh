20e67a61b647f93ca9ff51517a8f0e71f4bafc14
tony-dinh <tonyd@sfu.ca>
2017-01-23 16:29:51 -0800

    ✨ remove compile/transpiled code from repo

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -3,10 +3,13 @@ ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 VENDOR_DIR="${ROOT_DIR}/src/app/js/vendor/"
 
 # Make vendor directory if it doesn't exist
-echo $VENDOR_DIR
 [ -d $VENDOR_DIR ] || mkdir $VENDOR_DIR
-#
+
+echo ""
+echo ""
 echo "[ 💿  ] Copying dependencies to $VENDOR_DIR"
 echo "[ .. ] Working..."
 cp $ROOT_DIR/node_modules/jquery/dist/jquery.min.js $VENDOR_DIR
 echo "[ OK ] Dependencies successfully installed 🍻!"
+echo ""
+echo ""
